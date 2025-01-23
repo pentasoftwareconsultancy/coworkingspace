4de5096a67bbd80060ccce998fb88b43ee2f423a
Divya <divyakolsure090902@gmail.com>
2025-01-23 10:48:34 +0530

    app.js and navabr.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Footer from './components/footer/Footer';
 import WorkspaceListings from './components/card/WorkspaceListings';
 import Home from './pages/Home';
 import SeatingPlansAndFeatures from './pages/SeatingPlansAndFeatures';
+import Ameneties from './components/ameneties/Ameneties';
+import Benefits from './components/benefits/Benefits';
+import PricingPlan from './components/pricingplan/PricingPlan';
+import Contactus from './components/contactus/Contactus';
 
 
 function App() {
@@ -14,6 +18,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/amenities" element={<Ameneties />} />
+        <Route path="/benefits" element={<Benefits />} />
+        <Route path="/pricing" element={<PricingPlan />} />
+        <Route path="/contact" element={<Contactus />} />
         <Route path="/workspaceListing" element={<WorkspaceListings/>} />
         <Route path="/seatingplans/:id" element={<SeatingPlansAndFeatures/>} />
       </Routes>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
 import logo from '../../assets/images/coffice_logo.svg'; // Ensure you have a logo image in your project
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [scrolled, setScrolled] = useState(false);
@@ -24,11 +25,11 @@ const Navbar = () => {
                 <img src={logo} alt="Logo" />
             </div>
             <div className={styles.navLinks}>
-                <a href="#home" className={styles.navLink}>Home</a>
-                <a href="#amenities" className={styles.navLink}>Amenities</a>
-                <a href="#benefits" className={styles.navLink}>Benefits</a>
-                <a href="#pricing" className={styles.navLink}>Pricing</a>
-                <a href="#contact" className={styles.navLink}>Contact us</a>
+                <Link to="/" className={styles.navLink}>Home</Link>
+                <Link to="/amenities" className={styles.navLink}>Amenities</Link>
+                <Link to="/benefits" className={styles.navLink}>Benefits</Link>
+                <Link to="/pricing" className={styles.navLink}>Pricing</Link>
+                <Link to="/contact" className={styles.navLink}>Contact us</Link>
             </div>
             <a href="#book" className={styles.bookNowBtn}>Book Now</a>
         </nav>
